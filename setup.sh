755d53da985914c06c9e29436ddafe9eb601fda0
Samson Kirk-Koffi <koffisam@Samsons-MBP-2.local>
2017-05-12 15:09:09 -0400

    cleanup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 # (c) Copyright 2017 Samson Kirk-Koffi (samson@nxcartech.com)
 
 curl -LOk https://github.com/koffisam/nxCar-utils/archive/master.zip
-tar -xvzf master.zip 
+unzip  master.zip 
 cd nxCar-utils-master
 mv --backup=numbered config /etc/kbd/
 mv --backup=numbered dphys-swapfile /etc/
